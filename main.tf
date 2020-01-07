bc0e0c9386c077ff5b4caebba4c66043a6cc2a6c
Gilberto Castillo <gilberto.castillo@rogers.com>
2020-01-07 15:20:57 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ data "terraform_remote_state" "rstate" {
   backend = "remote"
   config = {
     workspaces =
-      hostname = ${TFE_HOST}
-      organization = ${TFE_ORG}
-      token = ${TFE_TOKEN}
+      hostname = "${TFE_HOST}"
+      organization = "${TFE_ORG}"
+      token = "${TFE_TOKEN}"
       name = "tfe-remote-parent"
     }
   }
