fcacfc0ba5268ea580b4249f29cd2db468fad91c
gcastill0 <gilberto@hashicorp.com>
2020-01-07 15:37:48 -0500

    removed a var

--- main.tf
+++ main.tf
@@ -4,15 +4,27 @@ variable "TFE_ORG" {}
 
 variable "TFE_TOKEN" {}
 
-
 data "terraform_remote_state" "rstate" {
   backend = "remote"
   config = {
     hostname     = "${TFE_HOST}"
     organization = "${TFE_ORG}"
-    token        = "${TFE_TOKEN}"
     workspaces = {
       name = "tfe-remote-parent"
     }
   }
+}
+
+# Configure the Azure Provider
+provider "azurerm" {
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  version = "=1.38.0"
+}
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "child_01" {
+  name                = "child_01_network"
+  resource_group_name = data.terraform_remote_state.rstate.outputs.azure_resource_group_name
+  location            = data.terraform_remote_state.rstate.outputs.azure_resource_group_location
+  address_space       = ["10.0.0.0/16"]
 }
